a65fa5a69794c587284ae3e7bc6d92fdf993fa56
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-04-25 13:44:18 +0200

    mic refresh rate

--- client/app/scripts/mic.js
+++ client/app/scripts/mic.js
@@ -5,11 +5,24 @@
     app.controller('micCtrl', ["$scope", "$http", 
         function ($scope, $http) {
             var vm = this;
-            $http.get("/api/mic/" + $scope.username).then(function(resp) {
-                vm.quarter = resp.data.quarter;
-                vm.PG1 = resp.data.PG1;
-                vm.PG2 = resp.data.PG2;
+            
+            function update() {
+                $http.get("/api/mic/" + $scope.username).then(function(resp) {
+                    vm.quarter = resp.data.quarter;
+                    vm.PG1 = resp.data.PG1;
+                    vm.PG2 = resp.data.PG2;
+                });
+            }
+            
+            var tick = $interval(update, 5*60*60*1000); // update every 5 hours
+            $scope.$on("$destroy", () => {
+                if (angular.isDefined(tick)) {
+                    $interval.cancel(tick);
+                    tick = undefined;
+                } 
             });
+            
+            update();
         }
     ]);
   
