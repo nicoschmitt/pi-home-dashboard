4256ee929434c425590a17d8212c40bb45b76c88
Nicolas Schmitt <nicolas.schmitt@gmail.com>
2016-05-05 10:34:05 +0200

    show travel to work only on work day

--- client/app/scripts/home.js
+++ client/app/scripts/home.js
@@ -1,10 +1,11 @@
 (function() {
-    
     var app = angular.module('myApp');
-  
-    app.controller('homeCtrl', ['$window', "$scope", "myConfig",
-        function ($window, $scope, $config) {
+    
+    app.controller('homeCtrl', ['$window', "$scope", "$interval", "myConfig",
+        function ($window, $scope, $interval, $config) {
             var vm = this;
+            vm.isWorkingDay = true;
+            
             if ($config.env == "development") {
                 vm.debug = $(window).width() + "x" + $(window).height();
                 $($window).resize(function() {
@@ -13,6 +14,20 @@
                     });
                 });
             }
+            
+            function update() {
+                vm.isWorkingDay = moment().startOf('day').isWorkingDay();
+            }
+            
+            var tick = $interval(update, 3*60*60*1000); // update every 3 hours
+            $scope.$on("$destroy", () => {
+                if (angular.isDefined(tick)) {
+                    $interval.cancel(tick);
+                    tick = undefined;
+                } 
+            });
+            
+            update();
         }
     ]);
   
