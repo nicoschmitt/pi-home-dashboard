ea4d50ff428b2f3e56f4cdba4fee682737abe15f
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-05-25 10:37:52 +0200

    Travel: better error handling

--- client/app/scripts/travel-time.js
+++ client/app/scripts/travel-time.js
@@ -1,9 +1,10 @@
 (function() {
     var app = angular.module('myApp');
     
-    app.controller('travelCtrl', ["$scope", "$http", "$interval", "myConfig",
-        function ($scope, $http, $interval, myConfig) {
+    app.controller('travelCtrl', ["$scope", "$http", "$interval", "$timeout", "myConfig",
+        function ($scope, $http, $interval, $timeout, myConfig) {
             var vm = this;
+            vm.warning = false;
             vm.routes = [];
             
             var from = myConfig.travelTime.from.latitude + "," + myConfig.travelTime.from.longitude;
@@ -11,6 +12,7 @@
             function update() {
                 var qs = "?from=" + from + "&to=" + to;
                 $http.get("/api/travel" + qs).then(function(resp) {
+                    vm.warning = false;
                     vm.routes = resp.data;
                     vm.routes.map(elt => {
                         elt.nicedist = (elt.dist / 1000).toFixed(2) + "km";
@@ -22,6 +24,11 @@
                         elt.nicetime += duration.minutes() + " min";
                         return elt;
                     });
+                }, function(resp) {
+                    // error
+                    console.log("error loading travel time data");
+                    vm.warning = true;
+                    $timeout(update, 30*1000); // retry in 30 seconds
                 });
             }
             
