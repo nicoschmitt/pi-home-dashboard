ddcb965547d4dd70cdd0e1e5df62d552a315f0a6
Nicolas Schmitt <nicolas.schmitt@gmail.com>
2016-07-03 12:37:21 +0200

    change for mic

--- server/api/mic/mic.controller.js
+++ server/api/mic/mic.controller.js
@@ -1,11 +1,21 @@
 (function(){
 
     var request = require("request");
+    var moment = require("moment");
   
     module.exports.getdata = function(req, res) {
         var mymic = process.env.MYMIC_URL;
         var key = process.env.MYMIC_SECRETKEY;
-        var url = mymic + "/api/data/" + req.params.username + "?key=" + key;
+
+        var quarter = "";
+        var month = moment().month();
+        if (month < 3) quarter = "Q3";
+        else if (month < 7) quarter = "Q4"; // July is like Q4
+        else if (month < 9) quarter = "Q1";
+        else quarter = "Q2";
+
+        var url = mymic + "/" + req.params.username + "?quarter=" + quarter + "&key=" + key;
+        console.log(url);
         
         request.get(url, (err, response, body) => {
              if (err) {
